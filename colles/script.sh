80ce3e0f097a78e480dd93c3e8ae319e9841adc7
cedric bounya <ece2larochelle@gmail.com>
2016-12-09 17:38:23 +0100

    programmes de colles semaine 10 + renommé tous les fichiers à deux chiffres

---
+++ colles/script.sh
@@ -0,0 +1,8 @@
+mv semaine1.html semaine01.html
+mv semaine2.html semaine02.html
+mv semaine3.html semaine03.html
+mv semaine4.html semaine04.html
+mv semaine5.html semaine05.html
+mv semaine6.html semaine06.html
+mv semaine7.html semaine07.html
+mv semaine8.html semaine08.html
